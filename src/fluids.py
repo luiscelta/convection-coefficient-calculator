0ac180838e0a22db4c24f5ba0b706332ea87b7c1
Luis <luis7.workspace@gmail.com>
2026-06-02 16:45:49 +0200

    Add FluidProperties class

---
+++ src/fluids.py
@@ -0,0 +1,10 @@
+class FluidProperties:
+    def __init__(self, density, specificHeat, dynamicViscosity, conductivity, surfaceTension, latentHeat):
+        self.density = density
+        self.specificHeat = specificHeat
+        self.dynamicViscosity = dynamicViscosity
+        self.conductivity = conductivity
+        self.surfaceTension = surfaceTension
+        self.latentHeat = latentHeat
+
+
